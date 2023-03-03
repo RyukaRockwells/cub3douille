cd5b4f38aa4275441e849b61f4fe1deacc3a4d1f
Nicole CHOW-YUEN-MIN <nchow-yu@e1r9p19.clusters.42paris.fr>
2023-03-03 13:40:13 +0100

    change a little details forget before like: corrd to coord

--- srcs/raycaster/fill_the_struct_for_render.c
+++ srcs/raycaster/fill_the_struct_for_render.c
@@ -6,7 +6,7 @@
 /*   By: nchow-yu <nchow-yu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 16:44:37 by nchow-yu          #+#    #+#             */
-/*   Updated: 2023/03/02 16:37:21 by nchow-yu         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:38:34 by nchow-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ t_fov	fill_the_struct_for_render(double rad, double d, char i, t_coord *coord, t
 	fov.degrees = rad * (180 / M_PI);
 	fov.dist = d;
 	fov.x = coord->x;
-	fov.y = corrd->y;
+	fov.y = coord->y;
 	fov.wall_orientation = '\0';
 	if (i == 'H')
 	{
