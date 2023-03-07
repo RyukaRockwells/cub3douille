aa7403da218cd7edfbe6f839c8551bd54bcbbf9a
Sunny ANAUTH <sanauth@e1r8p21.clusters.42paris.fr>
2023-03-07 08:52:06 +0100

    wip fix parsing ok

--- srcs/error/ft_error_params.c
+++ srcs/error/ft_error_params.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_error_params.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nchow-yu <nchow-yu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sanauth <sanauth@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 01:22:11 by nicole            #+#    #+#             */
-/*   Updated: 2023/02/24 13:04:28 by nchow-yu         ###   ########.fr       */
+/*   Updated: 2023/03/07 08:48:56 by sanauth          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ void	error_missing_params(t_data *data)
 
 void	ft_map_error(t_data *data, char *str)
 {
+	ft_free_all_params(data);
 	ft_putstr_fd(str, 2);
 	ft_free(data->file);
 	ft_free(data->map);
